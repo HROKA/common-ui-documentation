6bb5c5187aeb917b71445a656a06bfb2ce0c3552
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2021-08-12 21:58:43 +0300

    created first section

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -1,7 +1,8 @@
 import 'package:common_ui_toolkit/components/common_container/CommonContainer.dart';
 import 'package:common_ui_toolkit/index.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_countdown_timer/index.dart';
+
+import '../../utils/Constants.dart';
 
 class Home extends StatelessWidget {
   int endTime = DateTime(
@@ -18,32 +19,151 @@ class Home extends StatelessWidget {
       style: CommonContainerModel(
         width: DEVICE_WIDTH,
         height: DEVICE_HEIGHT,
-        paddingTop: DEVICE_HEIGHT * 0.1,
-        backgroundImage: DecorationImage(
-          image: NetworkImage(
-            'https://i.imgur.com/RLYUySY.png',
-          ),
-          fit: BoxFit.fill,
-        ),
       ),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            CommonText(
-              text: 'Common UI toolkit \n\n',
-              style: CommonTextStyles().h1Style().copyWith(
-                    fontColor: 0xFFFFFFFF,
-                    fontweight: FontWeight.bold,
-                    fontSize: 60,
+            CommonContainer(
+              style: CommonContainerModel(
+                backgroundImage: DecorationImage(
+                  image: NetworkImage(
+                    'https://i.imgur.com/PjztU0g.jpg',
                   ),
-            ),
-            CountdownTimer(
-              endTime: endTime,
-              textStyle: TextStyle(
-                fontSize: 60,
-                color: Color(0xFFFFFFFF),
+                  repeat: ImageRepeat.repeat,
+                ),
+                width: DEVICE_WIDTH,
+                height: DEVICE_HEIGHT * 0.9,
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: DEVICE_HEIGHT * 0.1,
+                    left: DEVICE_WIDTH * 0.15,
+                    child: Column(
+                      children: [
+                        CommonText(
+                          text: 'Making your Flutter apps',
+                          style: CommonTextStyles().h1Style().copyWith(
+                                fontweight: FontWeight.bold,
+                                fontSize: 60,
+                              ),
+                          inlineSpans: [
+                            TextSpan(
+                              text: ' look \nand feel native',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 60,
+                                color: Color(
+                                  SECONDARY_COLOR,
+                                ),
+                              ),
+                            ),
+                          ],
+                          containerStyle: CommonContainerModel(
+                            width: DEVICE_WIDTH * 0.4,
+                          ),
+                        ),
+                        CommonText(
+                          text:
+                              'Common UI toolkit is a high-quality, that covered in all major use-cases.',
+                          style: CommonTextStyles().h2Style(),
+                          containerStyle: CommonContainerModel(
+                            width: DEVICE_WIDTH * 0.4,
+                            marginTop: DEVICE_HEIGHT * 0.13,
+                          ),
+                        ),
+                        CommonContainer(
+                          style: CommonContainerModel(
+                            width: DEVICE_WIDTH * 0.3,
+                            marginTop: DEVICE_HEIGHT * 0.05,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CommonContainer(
+                                style: CommonContainerModel(
+                                  backgroundColor: SECONDARY_COLOR,
+                                  width: DEVICE_WIDTH * 0.1,
+                                  height: DEVICE_HEIGHT * 0.07,
+                                  alignment: Alignment.center,
+                                  touchEffect: TouchableEffect(
+                                    type: TouchTypes.scaleAndFade,
+                                  ),
+                                ),
+                                onPress: () {},
+                                child: CommonText(
+                                  text: 'Learn more',
+                                  style: CommonTextStyles().h3Style().copyWith(
+                                        fontColor: PRIMARY_COLOR,
+                                        fontweight: FontWeight.bold,
+                                      ),
+                                ),
+                              ),
+                              CommonContainer(
+                                style: CommonContainerModel(
+                                  backgroundColor: PRIMARY_COLOR,
+                                  width: DEVICE_WIDTH * 0.1,
+                                  height: DEVICE_HEIGHT * 0.07,
+                                  alignment: Alignment.center,
+                                  touchEffect: TouchableEffect(
+                                    type: TouchTypes.scaleAndFade,
+                                  ),
+                                ),
+                                onPress: () {},
+                                child: CommonText(
+                                  text: 'Docs',
+                                  style: CommonTextStyles().h3Style().copyWith(
+                                        fontColor: SECONDARY_COLOR,
+                                        fontweight: FontWeight.bold,
+                                      ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    child: CommonContainer(
+                      style: CommonContainerModel(
+                        backgroundColor: SECONDARY_COLOR,
+                        width: DEVICE_WIDTH,
+                        height: DEVICE_HEIGHT * 0.2,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: DEVICE_HEIGHT * 0.2,
+                    right: DEVICE_WIDTH * 0.05,
+                    child: Image.network(
+                      'https://i.imgur.com/oEHgNuI.png',
+                      width: DEVICE_WIDTH * 0.2,
+                      height: DEVICE_HEIGHT * 0.6,
+                    ),
+                  ),
+                  Positioned(
+                    top: DEVICE_HEIGHT * 0.09,
+                    right: DEVICE_WIDTH * 0.15,
+                    child: Image.network(
+                      'https://i.imgur.com/oEHgNuI.png',
+                      width: DEVICE_WIDTH * 0.2,
+                      height: DEVICE_HEIGHT * 0.8,
+                    ),
+                  ),
+                ],
               ),
             ),
+            CommonContainer(
+              style: CommonContainerModel(
+                backgroundColor: SECONDARY_COLOR,
+                width: DEVICE_WIDTH,
+                height: DEVICE_HEIGHT * 0.5,
+              ),
+              child: Stack(),
+            )
           ],
         ),
       ),
