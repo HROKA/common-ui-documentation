0cd1db588476e770ba078bf7f57bf6ecf491bb37
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2021-12-08 16:03:19 +0200

    removed # from the url using url_strategy pacakge :

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
-
 // for device preview
 // import 'package:flutter/foundation.dart';
 // import 'package:device_preview/device_preview.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import './screens/index.dart';
 // import '../controllers/AppLanguage.dart';
 
 void main() async {
+  setPathUrlStrategy();
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.transparent, // navigation bar color
     statusBarColor: Colors.transparent, // status bar color
